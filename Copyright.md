86b3934dea46fea72b18b9770460137131a2f54c
Robinayush <116380184+robinayush10@users.noreply.github.com>
2024-03-01 13:02:54 +0530

    Add files via upload

---
+++ Copyright.md
@@ -0,0 +1,23 @@
+## ©RTX GAMING @2023
+All rights reserved.
+
+The code, and content of this application are protected by international copyright laws. Unauthorized reproduction or distribution of this application or any portion of it may result in severe civil and criminal penalties and will be prosecuted to the maximum extent possible under the law.
+
+RTX GAMING retains all rights, including but not limited to the right to use, copy, distribute, and make derivative works of this application.
+
+For inquiries about licensing, please contact [RTX#2419].
+
+/**
+ ██████╗░████████╗██╗░░██╗           
+ ██╔══██╗╚══██╔══╝╚██╗██╔╝          
+ ██████╔╝░░░██║░░░░╚███╔╝░          
+ ██╔══██╗░░░██║░░░░██╔██╗░          
+ ██║░░██║░░░██║░░░██╔╝╚██╗          
+ ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
+
+  DISCORD SERVER : https://discord.gg/FUEHs7RCqz
+  YOUTUBE : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+ * **********************************************
+ *   Code by RTX GAMING
+ * **********************************************
+ */
---
+++ index.js
@@ -0,0 +1,75 @@
+const { Client, GatewayIntentBits, ActivityType, TextChannel } = require('discord.js');
+require('dotenv').config();
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const client = new Client({
+  intents: Object.keys(GatewayIntentBits).map((a) => {
+    return GatewayIntentBits[a];
+  }),
+});
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+  res.send('YaY Your Bot Status Changed✨');
+});
+app.listen(port, () => {
+  console.log(`🔗 Listening to Robin NTW : http://localhost:${port}`);
+  console.log(`🔗 Powered By Robinayush`);
+});
+
+
+const statusMessages = ["Robin NTW","/help"];
+
+
+let currentIndex = 0;
+const channelId = '';
+
+async function login() {
+  try {
+    await client.login(process.env.TOKEN);
+    console.log(`\x1b[36m%s\x1b[0m`, `|  ✅ Logged in as ${client.user.tag}`);
+  } catch (error) {
+    console.error('Failed to log in:', error);
+    process.exit(1);
+  }
+}
+
+
+
+
+function updateStatusAndSendMessages() {
+  const currentStatus = statusMessages[currentIndex];
+  const nextStatus = statusMessages[(currentIndex + 1) % statusMessages.length];
+
+  client.user.setPresence({
+    activities: [{ name: currentStatus, type: ActivityType.Custom}],
+    status: 'dnd',
+  });
+
+  
+  const textChannel = client.channels.cache.get(channelId);
+
+  if (textChannel instanceof TextChannel) {
+   
+    textChannel.send(`Bot status is: ${currentStatus}`);
+  } else {
+
+  }
+
+  currentIndex = (currentIndex + 1) % statusMessages.length;
+}
+
+client.once('ready', () => {
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ✅ Bot is ready as ${client.user.tag}`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    💉 Status Changing Code Injected Succesfully`);
+  updateStatusAndSendMessages();
+
+  setInterval(() => {
+    updateStatusAndSendMessages();
+  }, 10000);
+});
+
+login();
+
+
